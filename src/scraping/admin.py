5d0a3b58ceecf9e4e0d30b06f0c1931cd3b46acb
MaksimMalkovV <maer2971@gmail.com>
2023-05-01 21:58:41 +0300

    Доработка 0.1

---
+++ src/scraping/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import City, lenguage, Vacancy
+
+admin.site.register(City)
+admin.site.register(lenguage)
+admin.site.register(Vacancy)
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
